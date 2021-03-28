9c280e3b7cfb811d47dfbec71a8c7118414cb3a6
Tanner Villarete <tvillarete@gmail.com>
2021-03-28 13:29:35 -0700

    HomeView: Update to use MusicKit

--- src/App/views/HomeView/index.tsx
+++ src/App/views/HomeView/index.tsx
@@ -10,49 +10,50 @@ import ViewOptions, {
 } from 'App/views';
 import { SelectableList, SelectableListOption } from 'components';
 import { useScrollHandler } from 'hooks';
-import { useAudioService } from 'services/audio';
+import { useMusicKit } from 'hooks/useMusicKit';
 
 const strings = {
-  nowPlaying: "Now Playing"
+  nowPlaying: 'Now Playing',
 };
 
 const HomeView = () => {
   const initialOptions: SelectableListOption[] = [
     {
-      label: "Cover Flow",
+      label: 'Cover Flow',
       value: () => <CoverFlowView />,
       viewId: ViewOptions.coverFlow.id,
-      preview: PREVIEW.MUSIC
+      preview: PREVIEW.MUSIC,
     },
     {
-      label: "Music",
+      label: 'Music',
       value: () => <MusicView />,
       viewId: ViewOptions.music.id,
-      preview: PREVIEW.MUSIC
+      preview: PREVIEW.MUSIC,
     },
     {
-      label: "Games",
+      label: 'Games',
       value: () => <GamesView />,
       viewId: ViewOptions.games.id,
-      preview: PREVIEW.GAMES
+      preview: PREVIEW.GAMES,
     },
     {
-      label: "Settings",
+      label: 'Settings',
       value: () => <SettingsView />,
       viewId: ViewOptions.settings.id,
-      preview: PREVIEW.SETTINGS
-    }
+      preview: PREVIEW.SETTINGS,
+    },
   ];
 
   const [options, setOptions] = useState(initialOptions);
-  const { source } = useAudioService();
+  const { music, isConfigured } = useMusicKit();
   const [index] = useScrollHandler(ViewOptions.home.id, options);
 
   /** Append the "Now Playing" button to the list of options. */
   const showNowPlaying = useCallback(() => {
     if (
-      source &&
-      !options.find(option => option.label === strings.nowPlaying)
+      isConfigured &&
+      music.player?.nowPlayingItem &&
+      !options.find((option) => option.label === strings.nowPlaying)
     ) {
       setOptions([
         ...options,
@@ -60,27 +61,38 @@ const HomeView = () => {
           label: strings.nowPlaying,
           value: () => <NowPlayingView />,
           viewId: ViewOptions.nowPlaying.id,
-          preview: PREVIEW.NOW_PLAYING
-        }
+          preview: PREVIEW.NOW_PLAYING,
+        },
       ]);
     }
-  }, [options, source]);
+  }, [isConfigured, music.player?.nowPlayingItem, options]);
 
   /** Remove the "Now Playing" button from the list of options. */
   const hideNowPlaying = useCallback(() => {
-    if (options.find(option => option.label === strings.nowPlaying)) {
-      setOptions(options.filter(option => option.label !== strings.nowPlaying));
+    if (options.find((option) => option.label === strings.nowPlaying)) {
+      setOptions(
+        options.filter((option) => option.label !== strings.nowPlaying)
+      );
     }
   }, [options]);
 
   /** Conditionally show "Now Playing" button if music is queued/playing. */
   useEffect(() => {
-    if (source) {
+    if (!isConfigured) {
+      return;
+    }
+
+    if (music.player?.nowPlayingItem) {
       showNowPlaying();
     } else {
       hideNowPlaying();
     }
-  }, [hideNowPlaying, showNowPlaying, source]);
+  }, [
+    hideNowPlaying,
+    isConfigured,
+    music.player?.nowPlayingItem,
+    showNowPlaying,
+  ]);
 
   return <SelectableList options={options} activeIndex={index} />;
 };
