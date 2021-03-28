94285f13d21d8936eb2b72188065039c36d57afc
Tanner Villarete <tvillarete@gmail.com>
2021-03-28 13:30:14 -0700

    MusicView: Add coverflow option

--- src/App/views/MusicView/index.tsx
+++ src/App/views/MusicView/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 import { PREVIEW } from 'App/previews';
-import { AlbumsView, ArtistsView } from 'App/views';
+import {
+  AlbumsView,
+  ArtistsView,
+  CoverFlowView,
+  PlaylistsView,
+} from 'App/views';
 import { SelectableList, SelectableListOption } from 'components';
 import { useMenuHideWindow, useScrollHandler } from 'hooks';
 
@@ -11,17 +16,29 @@ const MusicView = () => {
   useMenuHideWindow(ViewOptions.music.id);
   const options: SelectableListOption[] = [
     {
-      label: "Artists",
-      value: () => <ArtistsView />,
-      viewId: ViewOptions.artists.id,
-      preview: PREVIEW.MUSIC
+      label: 'Cover Flow',
+      value: () => <CoverFlowView />,
+      viewId: ViewOptions.coverFlow.id,
+      preview: PREVIEW.MUSIC,
     },
     {
-      label: "Albums",
+      label: 'Artists',
+      value: () => <ArtistsView />,
+      viewId: ViewOptions.artists.id,
+      preview: PREVIEW.MUSIC,
+    },
+    {
+      label: 'Albums',
       value: () => <AlbumsView />,
       viewId: ViewOptions.albums.id,
-      preview: PREVIEW.MUSIC
-    }
+      preview: PREVIEW.MUSIC,
+    },
+    {
+      label: 'Playlists',
+      value: () => <PlaylistsView />,
+      viewId: ViewOptions.playlists.id,
+      preview: PREVIEW.MUSIC,
+    },
   ];
   const [index] = useScrollHandler(ViewOptions.music.id, options);
 
