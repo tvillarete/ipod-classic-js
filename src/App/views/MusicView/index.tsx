c1a4cf07b8c8f5fc2dbfa87c33aac35e6e715b8a
Tanner Villarete <tvillarete@gmail.com>
2021-03-30 15:34:03 -0700

    MusicView: Add NowPlaying button

--- src/App/views/MusicView/index.tsx
+++ src/App/views/MusicView/index.tsx
@@ -1,47 +1,67 @@
+import React, { useMemo } from 'react';
+
 import { PREVIEW } from 'App/previews';
 import {
   AlbumsView,
   ArtistsView,
   CoverFlowView,
+  NowPlayingView,
   PlaylistsView,
 } from 'App/views';
 import { SelectableList, SelectableListOption } from 'components';
-import { useMenuHideWindow, useScrollHandler } from 'hooks';
+import { useMenuHideWindow, useMusicKit, useScrollHandler } from 'hooks';
 
 import ViewOptions from '../';
 
 const MusicView = () => {
+  const { music } = useMusicKit();
   useMenuHideWindow(ViewOptions.music.id);
-  const options: SelectableListOption[] = [
-    {
-      type: 'View',
-      label: 'Cover Flow',
-      viewId: ViewOptions.coverFlow.id,
-      component: () => <CoverFlowView />,
-      preview: PREVIEW.MUSIC,
-    },
-    {
-      type: 'View',
-      label: 'Playlists',
-      viewId: ViewOptions.playlists.id,
-      component: () => <PlaylistsView />,
-      preview: PREVIEW.MUSIC,
-    },
-    {
-      type: 'View',
-      label: 'Artists',
-      viewId: ViewOptions.artists.id,
-      component: () => <ArtistsView />,
-      preview: PREVIEW.MUSIC,
-    },
-    {
-      type: 'View',
-      label: 'Albums',
-      viewId: ViewOptions.albums.id,
-      component: () => <AlbumsView />,
-      preview: PREVIEW.MUSIC,
-    },
-  ];
+
+  const options: SelectableListOption[] = useMemo(() => {
+    const arr: SelectableListOption[] = [
+      {
+        type: 'View',
+        label: 'Cover Flow',
+        viewId: ViewOptions.coverFlow.id,
+        component: () => <CoverFlowView />,
+        preview: PREVIEW.MUSIC,
+      },
+      {
+        type: 'View',
+        label: 'Playlists',
+        viewId: ViewOptions.playlists.id,
+        component: () => <PlaylistsView />,
+        preview: PREVIEW.MUSIC,
+      },
+      {
+        type: 'View',
+        label: 'Artists',
+        viewId: ViewOptions.artists.id,
+        component: () => <ArtistsView />,
+        preview: PREVIEW.MUSIC,
+      },
+      {
+        type: 'View',
+        label: 'Albums',
+        viewId: ViewOptions.albums.id,
+        component: () => <AlbumsView />,
+        preview: PREVIEW.MUSIC,
+      },
+    ];
+
+    if (music.isAuthorized && music.player?.nowPlayingItem?.isPlayable) {
+      arr.push({
+        type: 'View',
+        label: 'Now playing',
+        viewId: ViewOptions.nowPlaying.id,
+        component: () => <NowPlayingView />,
+        preview: PREVIEW.NOW_PLAYING,
+      });
+    }
+
+    return arr;
+  }, [music.isAuthorized, music.player?.nowPlayingItem?.isPlayable]);
+
   const [scrollIndex] = useScrollHandler(ViewOptions.music.id, options);
 
   return <SelectableList options={options} activeIndex={scrollIndex} />;
