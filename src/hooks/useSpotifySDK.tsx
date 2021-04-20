6b48de9af1a35e225e094d0e1481bd00d7d3762e
Tanner Villarete <tvillarete@gmail.com>
2021-04-19 18:40:09 -0700

    useSpotifySdk: Create hook
    This manages mounting, authenticating, and accessing the Spotify Web
    Playback SDK. There will be a separate hook for accessing the API

---
+++ src/hooks/useSpotifySDK.tsx
@@ -0,0 +1,154 @@
+import {
+  createContext,
+  memo,
+  useCallback,
+  useContext,
+  useRef,
+  useState,
+} from 'react';
+
+import * as Utils from 'utils';
+import * as SpotifyUtils from 'utils/spotify';
+
+import useEffectOnce from './useEffectOnce';
+import { useSettings } from './useSettings';
+
+export interface SpotifySDKState {
+  isMounted: boolean;
+  spotifyPlayer: Spotify.SpotifyPlayer;
+  accessToken?: string;
+  deviceId?: string;
+}
+
+export const SpotifySDKContext = createContext<SpotifySDKState>({} as any);
+
+export type SpotifySDKHook = SpotifySDKState & {
+  signIn: () => void;
+  signOut: () => void;
+};
+
+export const useSpotifySDK = (): SpotifySDKHook => {
+  const {
+    isSpotifyAuthorized,
+    setIsSpotifyAuthorized,
+    isAppleAuthorized,
+    setService,
+  } = useSettings();
+  const state = useContext(SpotifySDKContext);
+
+  /**
+   * Open the Spotify OAuth login page. Once authenticated, the user will be
+   * redirected back to the app.
+   */
+  const signIn = useCallback(() => {
+    if (!isSpotifyAuthorized) {
+      window.open(
+        `http://tannerv.ddns.net:3001/${Utils.isDev() ? 'login_dev' : 'login'}`,
+        '_self'
+      );
+    }
+
+    setService('spotify');
+  }, [isSpotifyAuthorized, setService]);
+
+  const signOut = useCallback(() => {
+    state.spotifyPlayer.disconnect();
+    setIsSpotifyAuthorized(false);
+
+    SpotifyUtils.removeExistingTokens();
+
+    // Change to apple music if available.
+    setService(isAppleAuthorized ? 'apple' : undefined);
+  }, [
+    isAppleAuthorized,
+    setIsSpotifyAuthorized,
+    setService,
+    state.spotifyPlayer,
+  ]);
+
+  return {
+    ...state,
+    signIn,
+    signOut,
+  };
+};
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const SpotifySDKProvider = ({ children }: Props) => {
+  const { setIsSpotifyAuthorized, setService } = useSettings();
+  const [token, setToken] = useState<string>();
+  const [deviceId, setDeviceId] = useState<string>();
+  const spotifyPlayerRef = useRef<Spotify.SpotifyPlayer | undefined>();
+  const [isMounted, setIsMounted] = useState(false);
+
+  /** Fetch access tokens and, if successful, then set up the playback sdk. */
+  const handleMount = useCallback(async () => {
+    const { accessToken, refreshToken, isNew } = await SpotifyUtils.getTokens();
+    setIsMounted(true);
+
+    if (accessToken && refreshToken) {
+      setToken(accessToken);
+
+      const player = new window.Spotify.Player({
+        name: 'iPod Classic',
+        getOAuthToken: async (cb: (token: string) => void) => {
+          const { storedAccessToken } = SpotifyUtils.getExistingTokens();
+
+          if (!storedAccessToken) {
+            console.error("ERROR: Didn't find stored access token");
+            return;
+          }
+
+          cb(storedAccessToken);
+        },
+      });
+
+      player.addListener('ready', ({ device_id }: any) => {
+        console.log({ device_id });
+        setDeviceId(device_id);
+        setIsSpotifyAuthorized(true);
+      });
+
+      player.addListener('authentication_error', ({ message }) => {
+        console.error(`Spotify authentication error: ${message}`);
+        setIsSpotifyAuthorized(false);
+      });
+
+      player.addListener('playback_error', ({ message }) => {
+        console.error(message);
+      });
+
+      player.connect();
+
+      spotifyPlayerRef.current = player;
+
+      /** The user has just signed in; configure the app to use Spotify. */
+      if (isNew) {
+        setService('spotify');
+      }
+    }
+  }, [setIsSpotifyAuthorized, setService]);
+
+  useEffectOnce(() => {
+    window.onSpotifyWebPlaybackSDKReady = handleMount;
+  });
+
+  return (
+    <SpotifySDKContext.Provider
+      value={{
+        spotifyPlayer:
+          spotifyPlayerRef.current ?? ({} as Spotify.SpotifyPlayer),
+        accessToken: token,
+        deviceId,
+        isMounted,
+      }}
+    >
+      {isMounted ? children : null}
+    </SpotifySDKContext.Provider>
+  );
+};
+
+export default memo(SpotifySDKProvider);
