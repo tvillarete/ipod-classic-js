1cade16949022c27f3d6d0093fca0e08ae8953df
Tanner Villarete <tvillarete@brex.com>
2024-11-16 16:00:47 -0800

    events: Add dispatch functions

--- app/utils/events.ts
+++ app/utils/events.ts
@@ -7,6 +7,16 @@ type BaseEventContext =
   | "menu"
   | "playpause";
 
+export type SupportedKeyCode =
+  | "ArrowUp"
+  | "ArrowDown"
+  | "ArrowLeft"
+  | "ArrowRight"
+  | "Escape"
+  | "Enter"
+  | " "
+  | "Spacebar";
+
 /** The action that is taken on a click-wheel control */
 type BaseEventAction = "click" | "longclick" | "scroll" | "longpress";
 
@@ -15,3 +25,70 @@ export type IpodEvent = `${BaseEventContext}${BaseEventAction}` | `idle`;
 
 /** Create a type-safe custom event for the iPod */
 export const createIpodEvent = (eventName: IpodEvent) => new Event(eventName);
+
+const backClickEvent = createIpodEvent("backwardclick");
+const backwardScrollEvent = createIpodEvent("backwardscroll");
+const centerClickEvent = createIpodEvent("centerclick");
+const centerLongClickEvent = createIpodEvent("centerlongclick");
+const forwardClickEvent = createIpodEvent("forwardclick");
+const forwardScrollEvent = createIpodEvent("forwardscroll");
+const idleEvent = createIpodEvent("idle");
+const menuClickEvent = createIpodEvent("menuclick");
+const menuLongPressEvent = createIpodEvent("menulongpress");
+const playPauseClickEvent = createIpodEvent("playpauseclick");
+const wheelClickEvent = createIpodEvent("wheelclick");
+
+export const dispatchMenuClickEvent = () =>
+  window.dispatchEvent(menuClickEvent);
+
+export const dispatchCenterClickEvent = () =>
+  window.dispatchEvent(centerClickEvent);
+
+export const dispatchCenterLongClickEvent = () =>
+  window.dispatchEvent(centerLongClickEvent);
+
+export const dispatchForwardScrollEvent = () =>
+  window.dispatchEvent(forwardScrollEvent);
+
+export const dispatchBackwardScrollEvent = () =>
+  window.dispatchEvent(backwardScrollEvent);
+
+export const dispatchWheelClickEvent = () =>
+  window.dispatchEvent(wheelClickEvent);
+
+export const dispatchMenuLongPressEvent = () =>
+  window.dispatchEvent(menuLongPressEvent);
+
+export const dispatchBackClickEvent = () =>
+  window.dispatchEvent(backClickEvent);
+
+export const dispatchForwardClickEvent = () =>
+  window.dispatchEvent(forwardClickEvent);
+
+export const dispatchPlayPauseClickEvent = () =>
+  window.dispatchEvent(playPauseClickEvent);
+
+export const dispatchIdleEvent = () => window.dispatchEvent(idleEvent);
+
+export const dispatchKeyboardEvent = (key: string) => {
+  switch (key) {
+    case "ArrowUp":
+    case "ArrowLeft":
+      dispatchBackwardScrollEvent();
+      break;
+    case "ArrowDown":
+    case "ArrowRight":
+      dispatchForwardScrollEvent();
+      break;
+    case "Enter":
+      dispatchCenterClickEvent();
+      break;
+    case " ":
+    case "Spacebar":
+      dispatchPlayPauseClickEvent();
+      break;
+    case "Escape":
+      dispatchMenuClickEvent();
+      break;
+  }
+};
